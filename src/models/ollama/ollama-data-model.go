36566fd76f0866d2754a9235fa26b86b29c09e5d
Olof Moriya <olof.moriya@gmail.com>
2025-12-29 22:49:16 +0900

    Adds ollama model

---
+++ src/models/ollama/ollama-data-model.go
@@ -0,0 +1,80 @@
+package ollama_model
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type RequestMessage struct {
+	Role    string           `json:"role"`
+	Content []RequestContent `json:"content"`
+}
+
+type ChatCompletionRequest struct {
+	Model     string           `json:"model"`
+	Messages  []RequestMessage `json:"messages"`
+	Tools     []Tool           `json:"tools"`
+	Stream    bool             `json:"stream"`
+	MaxTokens int              `json:"max_tokens,omitempty"`
+}
+
+type Tool interface {
+}
+
+type SimpleTool struct {
+	Type string `json:"type"`
+}
+
+type Choice struct {
+	Index        int         `json:"index"`
+	Message      Message     `json:"message"`
+	Logprobs     interface{} `json:"logprobs"`
+	FinishReason string      `json:"finish_reason"`
+}
+
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
+type ChatCompletion struct {
+	ID                string   `json:"id"`
+	Object            string   `json:"object"`
+	Created           int64    `json:"created"`
+	Model             string   `json:"model"`
+	SystemFingerprint string   `json:"system_fingerprint"`
+	Choices           []Choice `json:"choices"`
+	Usage             Usage    `json:"usage"`
+}
+
+type Delta struct {
+	Role    string `json:"role,omitempty"`
+	Content string `json:"content,omitempty"`
+}
+
+type RequestContent struct {
+	Type     string `json:"type"`
+	Text     string `json:"text,omitempty"`
+	ImageURL Image  `json:"image_url,omitempty"`
+}
+
+type Image struct {
+	URL string `json:"url"`
+}
+
+type ChatCompletionChunkChoice struct {
+	Index        int         `json:"index"`
+	Delta        Delta       `json:"delta"`
+	Logprobs     interface{} `json:"logprobs"`
+	FinishReason *string     `json:"finish_reason,omitempty"`
+}
+
+type ChatCompletionChunk struct {
+	ID                string                      `json:"id"`
+	Object            string                      `json:"object"`
+	Created           int64                       `json:"created"`
+	Model             string                      `json:"model"`
+	SystemFingerprint string                      `json:"system_fingerprint"`
+	Choices           []ChatCompletionChunkChoice `json:"choices"`
+}
